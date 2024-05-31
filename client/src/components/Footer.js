4f26e58ac13bd4a26abb7f332f17c9209736bab6
Omar4Belal <osameh@marun.edu.tr>
2024-05-31 08:45:08 +0300

    change

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,18 +6,6 @@ import Copyright from "./Copyright";
 const Footer = () => {
   return (
     <Box pb={3}>
-      <Card>
-        <Typography variant="subtitle1">
-          Don't forget to star the{" "}
-          <a
-            href="https://github.com/ihtasham42/social-media-app"
-            target="_blank"
-          >
-            Repo
-          </a>
-          ! ⭐
-        </Typography>
-      </Card>
     </Box>
   );
 };
